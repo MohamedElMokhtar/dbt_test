4af2675c8a9ae51a0dd21a7ad8588331e6c3bd36
MohamedElMokhtar <deroua.mohamed.mokhtar@gmail.com>
2025-12-07 09:09:55 +0100

    first

---
+++ models/transform_historique.sql
@@ -0,0 +1,29 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='src_id'
+    )
+}}
+
+with combined as (
+
+    select *, 'AHS' as src
+    from {{ source('public', 'ahs_historique') }}
+    where _ab_cdc_deleted_at is null
+
+    union all
+
+    select *, 'CSM' as src
+    from {{ source('public', 'csm_historique') }}
+    where _ab_cdc_deleted_at is null
+
+)
+
+select
+    *,
+    src || '_' || customerid as src_id
+from combined
+{% if is_incremental() %}
+  where _ab_cdc_updated_at >
+      (select max(_ab_cdc_updated_at) from {{ this }})
+{% endif %}
